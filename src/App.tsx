4d2d2843ff7525411d0dbe020f66f659e2a6242e
huangdi <huangdi_c@163.com>
2020-11-28 23:43:35 +0800

    基本 html 页面完成

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,33 @@ import Money from './views/Money';
 import Statistics from './views/Statistics';
 import Label from './views/Label';
 import NoMatch from './views/NoMatch';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/label">
-          <Label/>
-        </Route>
-        <Route path="/money">
-          <Money/>
-        </Route>
-        <Route path="/statistics">
-          <Statistics/>
-        </Route>
-        <Redirect exact from="/" to="/money"/>
-        <Route path="*">
-          <NoMatch/>
-        </Route>
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route path="/label">
+            <Label/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from="/" to="/money"/>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,63 @@
 import Layout from '../components/Layout';
 import React from 'react';
+import styled from 'styled-components';
+
+const LabelSection = styled.div`
+  
+`
+const NoteSection = styled.div`
+  
+`
+const CategorySection = styled.div`
+  
+`
+const NumberPadSection = styled.div`
+  
+`
 
 function Money() {
   return (
     <Layout>
-      <h2>记账页</h2>
+      <LabelSection>
+        <ol>
+          <li>衣服</li>
+          <li>食物</li>
+          <li>住宿</li>
+          <li>出行</li>
+        </ol>
+        <button>新增标签</button>
+      </LabelSection>
+      <NoteSection>
+        <label>
+          <span>备注</span>
+          <input type="text"/>
+        </label>
+      </NoteSection>
+      <CategorySection>
+        <ul>
+          <li>支出</li>
+          <li>收入</li>
+        </ul>
+      </CategorySection>
+      <NumberPadSection>
+        <div>100</div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>ok</button>
+          <button>0</button>
+          <button>.</button>
+        </div>
+      </NumberPadSection>
     </Layout>);
 }
 
