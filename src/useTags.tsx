03ed2761cb21ac504e271b56939da1518e38ea15
huangdi <huangdi_c@163.com>
2020-11-30 14:11:59 +0800

    tagList 样式

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 
-const useTags = () => {
+const useTags = () => { // 封装一个自定义 Hook
   const [tags, setTags] = useState<string[]>(['衣服', '吃饭', '住宿', '出行']);
   return {tags, setTags};
 };
