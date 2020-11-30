a5f34c055a56653c3d0dadb49f3d80849c8a7dd1
huangdi <huangdi_c@163.com>
2020-11-30 10:53:22 +0800

    收集表单数据 重构

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -9,7 +9,7 @@ import {NumberPadSection} from './Money/NumberPadSection';
 const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
-`
+`;
 type Category = '-' | '+'
 
 function Money() {
@@ -18,40 +18,24 @@ function Money() {
     note: '',
     category: '-' as Category,
     amount: 0
-  })
+  });
+  const onChange = (obj: Partial<typeof selected>) => {
+    setSelected({...selected, ...obj});
+  };
   return (
     <MyLayout>
-      {selected.tags}
-      <hr/>
-      {selected.note}
-      <hr/>
-      {selected.category}
-      <hr/>
-      {selected.amount}
       <LabelSection value={selected.tags}
-                    onChange={(tags)=>setSelected({
-                      ...selected,
-                      tags: tags
-                    })}
+                    onChange={tags=>onChange({tags})}
       />
       <NoteSection value={selected.note}
-                   onChange={(note)=>setSelected({
-                     ...selected,
-                     note:note
-                   })}
+                   onChange={note=>onChange({note})}
       />
       <CategorySection value={selected.category}
-                       onChange={(category)=>setSelected({
-                         ...selected,
-                         category: category
-                       })}
+                       onChange={category=>onChange({category})}
       />
       <NumberPadSection value={selected.amount}
-                        onChange={(amount)=>setSelected({
-                          ...selected,
-                          amount: amount
-                        })}
-                        onOk={()=>{}}
+                        onChange={amount=>onChange({amount})}
+                        onOk={() => {}}
       />
     </MyLayout>);
 }
