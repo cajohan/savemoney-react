138fa0728333d5b01ea69571f2471aafa8a762ea
huangdi <huangdi_c@163.com>
2020-12-01 11:26:33 +0800

    删除无用代码

--- src/views/Money/LabelSection.tsx
+++ src/views/Money/LabelSection.tsx
@@ -49,7 +49,7 @@ const LabelSection: React.FC<Props> = (props) => {
             () => {onToggleTag(tag.id);}
           } className={getClass(tag.id)}>{tag.name}</li>)}
       </ol>
-      <button onClick={()=>{console.log('2');addTag()}}>新增标签</button>
+      <button onClick={addTag}>新增标签</button>
     </Wrapper>
   );
 };
