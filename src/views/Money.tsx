23ec42bba9fa7ea1d14b6cf07a85ddc1fe85956d
huangdi <huangdi_c@163.com>
2020-12-01 11:25:45 +0800

    add record

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,23 +5,31 @@ import {LabelSection} from './Money/LabelSection';
 import {CategorySection} from './Money/CategorySection';
 import {NoteSection} from './Money/NoteSection';
 import {NumberPadSection} from './Money/NumberPadSection';
+import {useRecords} from '../hooks/useRecords';
 
 const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
 `;
 type Category = '-' | '+'
+const defaultFormData = {
+  tagIds: [] as number[],
+  note: '',
+  category: '-' as Category,
+  amount: 0
+}
 
 function Money() {
-  const [selected, setSelected] = useState({
-    tagIds: [] as number[],
-    note: '',
-    category: '-' as Category,
-    amount: 0
-  });
+  const [selected, setSelected] = useState(defaultFormData);
+  const {records, addRecord} = useRecords();
   const onChange = (obj: Partial<typeof selected>) => {
     setSelected({...selected, ...obj});
   };
+  const submit = ()=>{
+    addRecord(selected)
+    alert('保存成功')
+    setSelected(defaultFormData)
+  }
   return (
     <MyLayout>
       <LabelSection value={selected.tagIds}
@@ -35,7 +43,7 @@ function Money() {
       />
       <NumberPadSection value={selected.amount}
                         onChange={amount=>onChange({amount})}
-                        onOk={() => {}}
+                        onOk={submit}
       />
     </MyLayout>);
 }
