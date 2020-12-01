15bf5014ba4f4aa43a4f8ec07b9adbc0096addc4
huangdi <huangdi_c@163.com>
2020-12-01 20:56:01 +0800

    如果是电脑打开，跳提示并且限制最宽500px

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,11 @@ import App from './App';
 import 'index.scss'
 
 ReactDOM.render(
-
     <App />
   ,
   document.getElementById('root')
 );
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机打开本页面，以保证浏览效果');
+}
 
